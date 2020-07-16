99e69cb1257fcb53f3593dac056e04be8ddfb3f9
ilinoa <ilinoa90@gmail.com>
2020-07-16 16:11:31 +0300

    update

--- js/app.js
+++ js/app.js
@@ -1,50 +1,54 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+
+
+    const carouselSlide = document.querySelector('.carousel-slide')
+    const carouselImage = document.querySelectorAll('.carousel-slide img')
+
+    const prevBtn = document.querySelector('#prevBtn')
+    const nextBtn = document.querySelector('#nextBtn')
+
     
-const carouselSlide = document.querySelector('.carousel-slide')
-const carouselImage = document.querySelectorAll('.carousel-slide img')
-  
-const prevBtn = document.querySelector('#prevBtn')
-const nextBtn = document.querySelector('#nextBtn')
 
-const size = carouselImage[0].clientWidth
-let counter = 1
-const positonReal = "transform 0.4s ease-in-out"
+    const img = document.querySelector('.carousel-slide img')
 
-function positionPX(counter) {
-    var value = -(carouselImage[0].clientWidth * counter)
-    return 'translateX(' + value + 'px)'
-}
+    img.onload = function () {
 
+        function positionPX(counter) {
+            var value = -(carouselImage[0].clientWidth * counter)
+            return 'translateX(' + value + 'px)'
+        }
+        let counter = 1
 
-carouselSlide.style.transform = positionPX(counter)
-
- 
-nextBtn.addEventListener('click', () => {
-    if(counter >= carouselImage.length - 1) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out"
-    counter++;
-    carouselSlide.style.transform = positionPX(counter)
-})
-
-prevBtn.addEventListener('click', () => {
-    if(counter <= 0) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out"
-    counter--;
-    carouselSlide.style.transform = positionPX(counter)
-})
-
-
-carouselSlide.addEventListener('transitionend', () => {
-    if (carouselImage[counter].id === 'lastClone') {
-        carouselSlide.style.transition = "none"
-        counter = carouselImage.length - 2
         carouselSlide.style.transform = positionPX(counter)
-    }
 
-    if (carouselImage[counter].id === 'firstClone') {
-        carouselSlide.style.transition = "none"
-        counter = carouselImage.length - counter
-        carouselSlide.style.transform = positionPX(counter)
+
+        nextBtn.addEventListener('click', () => {
+            if (counter >= carouselImage.length - 1) return;
+            carouselSlide.style.transition = "transform 0.4s ease-in-out"
+            counter++;
+            carouselSlide.style.transform = positionPX(counter)
+        })
+
+        prevBtn.addEventListener('click', () => {
+            if (counter <= 0) return;
+            carouselSlide.style.transition = "transform 0.4s ease-in-out"
+            counter--;
+            carouselSlide.style.transform = positionPX(counter)
+        })
+
+
+        carouselSlide.addEventListener('transitionend', () => {
+            if (carouselImage[counter].id === 'lastClone') {
+                carouselSlide.style.transition = "none"
+                counter = carouselImage.length - 2
+                carouselSlide.style.transform = positionPX(counter)
+            }
+
+            if (carouselImage[counter].id === 'firstClone') {
+                carouselSlide.style.transition = "none"
+                counter = carouselImage.length - counter
+                carouselSlide.style.transform = positionPX(counter)
+            }
+        })
     }
-})
 })
